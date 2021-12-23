4725b523e3e8a43dc04475e4230840fb8d5bea0a
Sabbir <sabbir@Sabbirs-MacBook-Pro-16.local>
2021-12-23 11:47:19 +0600

    initial commit

---
+++ main.js
@@ -0,0 +1,53 @@
+const { app, BrowserWindow, ipcMain, nativeTheme } = require("electron");
+const path = require("path");
+const glasstron = require("glasstron");
+
+app.commandLine.appendSwitch("enable-transparent-visuals");
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+var mainWindow = null;
+glasstron
+// Quit when all windows are closed.
+app.on("window-all-closed", function () {
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform != "darwin") {
+		app.quit();
+	}
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on("ready", function () {
+	// Create the browser window.
+	mainWindow = new glasstron.BrowserWindow({
+    frames: false,
+    titleBarStyle: 'hidden',
+		width: 900,
+		height: 500,
+		"min-width": 800,
+		"min-height": 400,
+		backgroundColor: "transparent",
+		"accept-first-mouse": true,
+		"title-bar-style": "hidden",
+	});
+
+	// and load the index.html of the app.
+	mainWindow.loadFile("index.html");
+
+	// Open the DevTools.
+	//mainWindow.openDevTools();
+
+	// Emitted when the window is closed.
+	mainWindow.on("closed", function () {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null;
+	});
+
+  mainWindow.setVibrancy("fullscreen-ui");
+
+  mainWindow.setBlur(true);
+});
